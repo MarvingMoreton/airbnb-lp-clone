ff146327c1f019a9922556de129aec75dc67e5b8
Marving Moreton <moreton.marving@gmail.com>
2022-09-11 16:19:18 -0400

    airbnb-lp-clone-v1-finished

--- components/Card.jsx
+++ components/Card.jsx
@@ -3,27 +3,28 @@ import React from "react"
 
 
 export default function Card(props) {
-  console.log(props.openSpots)
+  // console.log(props.openSpots)
+  // console.log(props.item)
 // conditionning if openSpots === 0 display SOLD OUT badge OR if location === online display badge "ONLINE"
   let badgeText
-  if (props.openSpots === 0) {
+  if (props.item.openSpots === 0) {
     badgeText = "SOLD OUT"
-  } else if (props.location === "Online") {
+  } else if (props.item.location === "Online") {
     badgeText = "ONLINE"
   }
 
     return (
       <div className="card">
         {badgeText &&  <div className="card--badge">{badgeText}</div>}
-        <img src={`./public/assets/${props.img}`} className="card--image" />
+        <img src={`./public/assets/${props.item.coverImg}`} className="card--image" />
         <div className="card--stats">
             <i className="fa-solid fa-star card--star"></i>
-            <span>{props.rating}</span>
-            <span className="gray">({props.reviewCount}) •</span>
-            <span className="gray">{props.location}</span>
+            <span>{props.item.stats.rating}</span>
+            <span className="gray">({props.item.stats.reviewCount}) •</span>
+            <span className="gray">{props.item.location}</span>
         </div>
-        <p className="card--title">{props.title}</p>
-        <p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
+        <p className="card--title">{props.item.title}</p>
+        <p className="card--price"><span className="bold">From ${props.item.price}</span> / person</p>
     </div>
     )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-// import React from "react"
 import reactLogo from '../public/assets/react.svg'
 import './App.css'
 import Navbar from '../components/Navbar.jsx'
@@ -14,16 +13,10 @@ export default function App() {
   const cardElements = data.map(item => {
       return (
             <Card
-            // adding a prop called key set to sth unique about this individual item. Here everything is uique (except rating) but in order for it to work no matter what's the set up --> use the id of the item
+            // adding a prop called key set to sth unique about this individual item.
+            //Here everything is uique (except rating) but in order for it to work no matter what's the set up --> use the id of the item
             key={item.id}
-            img={item.coverImg}
-            rating={item.stats.rating}
-            reviewCount={item.stats.reviewCount}
-            location={item.location}
-            title={item.title}
-            // description={item.description}
-            price={item.price}
-            openSpots={item.openSpots}
+            item={item}
             />
       )
   })
